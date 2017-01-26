b6b1cfdaa852cfc320b3bbeabe4a27daa76c4ef0
katsoohoo <katherinesoohoo@gmail.com>
2017-01-26 14:37:18 -0800

    Add readme, update to secure links

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ $(document).ready(function() {
 
     $.ajax({
     method:'GET',
-    url:'http://services.faa.gov/airport/status/SFO?format=application/json',
+    url:'https://services.faa.gov/airport/status/SFO?format=application/json',
     success: function(data){
 
         console.log(data);
-        
+
         var message = "";
         var randNum = 0;
-        
-        var noDelayMsg = 
+
+        var noDelayMsg =
 ["Relax, your fucking flight is on time. Unless you're running late. Then fucking get to the airport ASAP.", "Karl the Fog doesn't have shit on us. NOT TODAY", "FUCK YEAH FLIGHT IS ON TIME WOO", "Karl ain't fuckin' delaying this airport!", "FUCK NO, Karl the Fog and friends ain't delaying shit.", "FUCK YEAH! As in, fuck yeah, there aren't any delays." ] ;
-        
+
         var delayMsg = ["FUCK. Karl strikes again.", "God damnit, shit's delayed, yo.", "UGH, FUCK MY ASS there's delays.", "KARLLLLLL FUCKKKK YOUUUUU we're delayed.", "shit. Shit. SHIT FUCK DAMNIT WEATHER DELAYS FUCK", "Karl, stop being such a dick and leave SFO alone!", "Karl, go die in a fire (can fog die in a fire?)"];
-        
+
         console.log(data.delay);
         if(data.delay == "true") {
             randNum = Math.floor(Math.random()*delayMsg.length);
@@ -35,7 +35,7 @@ $(document).ready(function() {
             randNum = Math.floor(Math.random()*noDelayMsg.length);
             message = noDelayMsg[randNum];
         }
-        
+
         $("#airport-name").html(data.name);
         $("#delay-message").html(message);
         $("#weather").html(data.weather.weather);
@@ -45,4 +45,3 @@ $(document).ready(function() {
     });
 
 });
-
